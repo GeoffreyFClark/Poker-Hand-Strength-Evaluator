c3a26eadfeb1f7beaaf294328c6d8061f3beecf9
Geoffrey F. Clark <97141856+GeoffreyFClark@users.noreply.github.com>
2023-11-02 20:09:11 -0600

    Formatted+added card images, deselect buttons, CSS

--- main.py
+++ main.py
@@ -2,24 +2,31 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
 
-# TO DO: Using console for now until GUI is fully working. 
+# TO DO: Using console for now until GUI is fully working.
+
+
 def get_user_input():
     print("""Card input format: <rank><suit>. Rank = 2-9, J, Q, K, A; Suit = H, D, S, C. \n
           e.g. 2 of Hearts = 2H; Ace of Diamonds = AD, Jack of Spades = JS, 10 of Clubs = 10C.""")
 
-    hand_cards = [input("Enter your first card: "), input("Enter your second card: ")]
-    table_cards = input("Enter table cards (comma separated, e.g. 2H,3D,4C, leave blank for none): ").split(',')
-    num_opponents = int(input("Enter number of opponents (leave blank for none): ") or 0)
+    hand_cards = [input("Enter your first card: "),
+                  input("Enter your second card: ")]
+    table_cards = input(
+        "Enter table cards (comma separated, e.g. 2H,3D,4C, leave blank for none): ").split(',')
+    num_opponents = int(
+        input("Enter number of opponents (leave blank for none): ") or 0)
     return hand_cards, table_cards, num_opponents
 
 
 def algorithm1(hand_cards, table_cards=[], num_opponents=0):
     return "Output from algorithm1"
+
 
 def algorithm2(hand_cards, table_cards=[], num_opponents=0):
     return "Output from algorithm2"
@@ -31,7 +38,8 @@ def index():
     output = ""
     if request.method == 'POST':
         hand_cards = [request.form.get('hand1'), request.form.get('hand2')]
-        table_cards = [request.form.get(f'table{i}') for i in range(1, 6) if request.form.get(f'table{i}')]
+        table_cards = [request.form.get(f'table{i}') for i in range(
+            1, 6) if request.form.get(f'table{i}')]
 
         if not all(hand_cards):
             output = "Please enter your hand cards."
@@ -45,6 +53,7 @@ def index():
                 output = algorithm2(hand_cards, table_cards, num_opponents)
 
     return render_template('index.html', output=output)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
