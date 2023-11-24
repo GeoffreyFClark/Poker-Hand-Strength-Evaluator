4c703a411100afe6e4407dc53a4012cb83c5a1a0
Geoffrey F. Clark <97141856+GeoffreyFClark@users.noreply.github.com>
2023-11-23 21:58:29 -0700

    frontend input/output done, comments/readme

--- README.md
+++ README.md
@@ -5,8 +5,9 @@
 ## TO DO:
 
 Backend:
-- Implement algorithm1 and algorithm2. (Rename for clarity once we've agreed upon algorithms to use)
-- Ensure input from the frontend is properly processed as parameters for our algorithm functions
+- Implement algorithm1 and algorithm2. (Can rename for clarity once we've agreed upon algorithms to use based on assignment requirements)
+ -- Two comparable combinatorial approaches? e.g. 1. Iterative Approach Using Hash Tables vs 2. Recursive Approach Using Graphs
+
 
 Frontend:
 - ~~Debug missing AD card in the selection GUI~~ Evidently, my Google Chrome adblock extension was somehow interfering with the AD card specifically haha
@@ -14,5 +15,6 @@ Frontend:
 - ~~Gray out selected cards (e.g., change to a white rectangle)~~
 - ~~Populate the selected card images into the "Your Hand" and "Table Cards" input boxes as opposed to 2 alphanumeric characters in boxes~~
 - ~~Allow removal of a card by clicking on populated card image instead of the current "X" system~~
+- ~~Ensure input from the frontend is properly processed as parameters for our algorithm functions~~
+- ~~Display the results once the "CALCULATE!" button is clicked~~
 - Add further CSS styling
-- Display the results once the "CALCULATE!" button is clicked (pending completion of algorithm1 and algorithm2)
--- main.py
+++ main.py
@@ -1,40 +1,27 @@
 from flask import Flask, render_template, request
 
-# Flask app initialization
 app = Flask(__name__)
 
-# Card class to store card rank and suit, perhaps add further class methods 
+
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
 
-# Console inputs initially used before GUI inputs implemented, 
-# kept for reference, but planning to remove/archive in future.
-def get_user_input():
-    print("""Card input format: <rank><suit>. Rank = 2-9, J, Q, K, A; Suit = H, D, S, C. \n
-          e.g. 2 of Hearts = 2H; Ace of Diamonds = AD, Jack of Spades = JS, 10 of Clubs = 10C.""")
 
-    hand_cards = [input("Enter your first card: "),
-                  input("Enter your second card: ")]
-    table_cards = input(
-        "Enter table cards (comma separated, e.g. 2H,3D,4C, leave blank for none): ").split(',')
-    num_opponents = int(
-        input("Enter number of opponents (leave blank for none): ") or 0)
-    return hand_cards, table_cards, num_opponents
+# Two different types of combinatorial analysis
 
-
-# Placeholder functions for our evaluation algorithms. 
-# We can discuss how to approach these as a group.
+# Iterative Approach Using Hash Tables
 def algorithm1(hand_cards, table_cards=[], num_opponents=0):
     return "Output from algorithm1"
 
+
+# Recursive Approach Using Graphs
 def algorithm2(hand_cards, table_cards=[], num_opponents=0):
     return "Output from algorithm2"
 
 
-# Flask routes to handle both GET and POST requests.
-# The main logic for the web app happens here, 
+# Flask routes to handle GET and POST requests.
 # Take user inputs, call algorithms, display result
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -44,10 +31,11 @@ def index():
         hand_cards = [request.form.get('hand1'), request.form.get('hand2')]
         table_cards = [request.form.get(f'table{i}') for i in range(
             1, 6) if request.form.get(f'table{i}')]
+        num_opponents = int(request.form.get('opponents') or 0)
 
         # check if both hand cards were entered
         if not all(hand_cards):
-            output = "Please enter your hand cards."
+            output = "Please enter both of your hand cards."
         else:
             evaluation_method = request.form.get('evaluation')
             num_opponents = int(request.form.get('opponents') or 0)
@@ -60,6 +48,7 @@ def index():
 
     # Render the main HTML page with any output
     return render_template('index.html', output=output)
+
 
 # Run Flask app. Debug mode currently on.
 if __name__ == "__main__":
